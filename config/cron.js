dd3c5fa42dd92b75793ade6b2e29a3f610055209
Satish Verma <67960740+satishbluone@users.noreply.github.com>
2020-10-28 17:07:26 +0530

    Update cron.js

--- config/cron.js
+++ config/cron.js
@@ -93,8 +93,7 @@ const runAsyncPublishPost = async (newsList) => {
 const publish_AutoScheduleNews = async (news) =>{
   let result = await News.update({ id: news.id }).set({
     // publishedAt: new Date()
-    publishedAt: news.scheduledTo,
-    dated: news.scheduledTo,
+    publishedAt: news.dated ? news.dated : moment().format("YYYY-MM-DD hh:mm:ss"),
     status: 'published'
   }).fetch();
   console.log('auto scheduled to published : ', result.length);
